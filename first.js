742f5c09e265b95478ce49c6acbd33e655910c27
Val33k <valentyn7879@gmail.com>
2022-08-07 17:54:19 +0200

    simple http server

--- first.js
+++ first.js
@@ -1,1 +1,8 @@
-console.log("Hello!");
+const http = require("http");
+http.createServer(function(request,response){
+
+    response.end("Hello NodeJS! Headers: " + JSON.stringify(request.headers));
+
+}).listen(80, "127.0.0.1",function(){
+    console.log("Сервер начал прослушивание запросов на порту 80");
+});
